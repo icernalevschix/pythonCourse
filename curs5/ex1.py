c2eadb843a5f605772e9593d03afae1115fa775d
Ion Cernalevschi <icernalevschix@eagleinvsys.com>
2019-11-14 08:28:36 -0500

    #5

--- curs5/ex1.py
+++
@@ -1,8 +0,0 @@
-s = "spam"
-
-for index, i in enumerate(s):
-    print(index, i)
-
-
-for i in enumerate("wtf"):
-    print(i)
--- curs5/ex8.py
+++ curs5/ex8.py
@@ -1,5 +1,5 @@
 # 4. Scrieti un program care sa elimine cuvinte duplicate dintr-o propozitie.
 
-str1 = list(input("Introduceti propozitia: \n").split())
+str1 = input("Introduceti propozitia: \n").split()
 
 print(" ".join([i for n,i in enumerate(str1) if i not in str1[:n]]))

0552583d124e122999839e8f7bdabca351ac1495
IC <cernolevschi@outlook.com>
2020-02-24 13:55:37 +0200

    project work

--- curs16/django_project/data_scraper/data_scraper/spiders/data_scraper.py
+++ curs16/django_project/data_scraper/data_scraper/spiders/data_scraper.py
@@ -22,25 +22,15 @@ class JobsSpider(scrapy.Spider):
         base_url = 'https://www.rabota.md'
 
         for job in response.css('.vacancy-block'):
-        #     item = PostItem()
-        #     item['title'] = job.css('.vacancy-block__name::text').get()
-        #     # item['author'] = job.css('.vacancy-block__company-name span::text').get()
-        #     item['author'] = User(id=1)
             title = job.css('.vacancy-block__name::text').get()
-            next_page = job.css('.vacancy-block__heading a::attr(href)').getall()[1]
             sallary  =job.css('.vacancy-block__salary::text').get().strip()
-        #     content = yield scrapy.Request(url=base_url + next_page, callback=self.parse_content)
-        #     print('********* content=', content)
 
-        #     item['content'] = job.css('.vacancy-block__requirements::text').get().strip().replace('\r\n', '')
-        #     item['sallary'] = job.css('.vacancy-block__salary::text').get().strip()
-
+            next_page = job.css('.vacancy-block__heading a::attr(href)').getall()[1]
             request = scrapy.Request(base_url + next_page,
                             callback=self.parse_content)
             request.cb_kwargs['title'] = title
             request.cb_kwargs['sallary'] = sallary
             yield request
-            # yield item
         
         next_page = response.xpath("//*[contains(@class, 'active')]/following-sibling::a/@href").get()
         
